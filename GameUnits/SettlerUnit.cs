e33e4dcc5f10bf093d7306d268456a2e9875e51b
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-05-05 11:12:26 +0100

    Modify SettlerUnit

--- GameUnits/SettlerUnit.cs
+++ GameUnits/SettlerUnit.cs
@@ -4,6 +4,17 @@ namespace GameUnits
 {
     public class SettlerUnit : Unit
     {
+        public SettlerUnit() : base(1,2)
+        {
+          public override float Cost
+          {
+            get
+            {
+                return 5;
+            }
+          }
+      
+        }
         public void Settle()
         {
             
