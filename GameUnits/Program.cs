fee5560584d0d2ed8a8741ef2a36f0fcafb619c4
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-05-05 12:00:01 +0100

    Replace Program

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -2,13 +2,43 @@
 
 namespace GameUnits
 {
-    class Program
+    public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            
-            
+            // Criar array com três unidades
+            Unit[] units = new Unit[]
+            {
+                new MilitaryUnit(3, 10, 5),
+                new MilitaryUnit(4, 5, 3),
+                new SettlerUnit(),
+                new SpyUnit(),
+                new SpyUnit()
+            };
+
+            // Unidade 0 ataca unidade 4
+            (units[0] as MilitaryUnit).Attack(units[4]);
+            // Unidade 0 ataca unidade 2
+            (units[0] as MilitaryUnit).Attack(units[2]);
+            // Unidade 3 espia unidade 4
+            (units[3] as SpyUnit).GetSecretInfo(units[4]);
+            // Unidade 4 espia unidade 2
+            (units[4] as SpyUnit).GetSecretInfo(units[2]);
+
+            // "Imprimir" cada unidade
+            // chamando implicitamente o método ToString() de cada uma delas
+            foreach (Unit u in units)
+            {
+                Console.WriteLine(u);
+            }
+
+            // Output esperado:
+            //
+            // MilitaryUnit: HP=12 COST=7 XP=2 AP=5
+            // MilitaryUnit: HP=5 COST=3 XP=0 AP=3
+            // SettlerUnit: HP=1 COST=5
+            // SpyUnit: HP=2 COST=12.5 XP=3
+            // SpyUnit: HP=1 COST=12.5 XP=1
         }
-        
     }
 }
---
+++ GameUnits/XPUnit.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace GameUnits
+{
+    public class XPUnit : Unit
+    {
+        public int XP {get; protected set;}
+
+        public XPUnit()
+        {
+            
+            XP=0;
+
+        }
+        
+    }
+}
